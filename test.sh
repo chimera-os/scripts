8f384cfd1dbcde71ac678c6659a23d4583d0bbc8
Nicolas LESQUOY <nicolas.lesquoy@gmail.com>
2022-12-30 10:28:10 +0100

    Update

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PATHTOLIST="./packages.list"
+
+# Fail on error and report it, debug all lines
+set -eu -o pipefail
+
+# Check if script is run as sudo
+USER=$(id -u)
+test $USER -eq 0 || echo "You should have sudo privilege to run this script"
+
+echo "Ready to install the following packages:"
+cat packages.list
+
+while true; do
+    read -p "Do you wish to continue?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+cat packages.list | xargs apt-get install -y
